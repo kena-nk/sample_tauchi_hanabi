f8c737618e542beacdfacd254c68e0c27f0f01f5
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2021-03-27 18:05:22 +0900

    カラーパレットを設定した

---
+++ themes/index.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    palette: {
+        main: '#cb2938',
+        bg: '#ececec',
+        accent: '#2b2b2b',
+        line: '#c1c1c1',
+    }
+};
+
+export type paletteKey = keyof typeof theme.palette;
